9b40f3896da0cc4f83306d106ce4f7016e82c50a
chrisjwwalker <cjw@walker54.plus.com>
2018-07-06 09:14:10 +0100

    Dice rolling app, with random number generation and linked UI components

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    let dies = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    func rollDice() {
+        diceImageViewOne.image = UIImage(named: dies[Int(arc4random_uniform(6))])
+        diceImageViewTwo.image = UIImage(named: dies[Int(arc4random_uniform(6))])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollDice()
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rollDice()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+        
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        rollDice()
+    }
 }
-
